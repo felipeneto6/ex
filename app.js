9634ddcc88c5980a6410ce12a68aa7697ce0d5b9
Felipe Gonçalves Neto <98574879+felipeneto6@users.noreply.github.com>
2022-03-31 05:44:26 -0300

    adicionado navagação mobile/header

---
+++ app.js
@@ -0,0 +1,25 @@
+const hamburguer = document.querySelector('.header .nav-bar .nav-list .hamburguer');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburguer.addEventListener('click',()=> {
+    hamburguer.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll',()=> {
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = "#29323c";
+    }else{
+        header.style.backgroundColor = "transparent";
+    }
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click',() => {
+        hamburguer.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
